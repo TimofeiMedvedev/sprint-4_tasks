1e4270c836a7183add14daa47e0011cdee1d294a
Timofei Medvedev <medtv78@gmail.com>
2024-07-03 16:45:41 +0300

    Add pointer

--- find_number_platforms.py
+++ find_number_platforms.py
@@ -4,29 +4,29 @@
 def find_number_platforms(
         weight_robots: list[int], limit_platform: int) -> int:
     """Обявляем функцию для подсчёта тележек."""
-    list.sort(weight_robots)
-    left_pointer: int = 0
-    right_pointer: int = len(weight_robots) - 1
+    sort_weight_robots = sorted(weight_robots)
+    light_robot: int = 0
+    heavy_robot: int = len(sort_weight_robots) - 1
     counter: int = 0
-    while left_pointer <= right_pointer:
-        s: int = weight_robots[left_pointer] + weight_robots[right_pointer]
-        if s <= limit_platform:
+    while light_robot <= heavy_robot:
+        weight_two_robots: int = sort_weight_robots[
+            light_robot] + sort_weight_robots[heavy_robot]
+        if weight_two_robots <= limit_platform:
             counter += 1
-            left_pointer += 1
-            right_pointer -= 1
-        if s > limit_platform:
+            light_robot += 1
+            heavy_robot -= 1
+        else:
             counter += 1
-            right_pointer -= 1
+            heavy_robot -= 1
     return counter
 
 
-weight_robots = [int(x) for x in input().split()]
-limit_platform = int(input())
+# weight_robots = [int(x) for x in input().split()]
+# limit_platform = int(input())
 # weight_robots = [1, 2]
 # limit_platform = 3
-# weight_robots = [3, 5, 3, 4]
-# limit_platform = 5
-# weight_robots = [50, 50, 50, 50]
-# limit_platform = 100
+weight_robots = [3, 5, 3, 4]
+# limit_platform = 5weight_robots = [50, 50, 50, 50]
+limit_platform = 5
 if __name__ == '__main__':
     print(find_number_platforms(weight_robots, limit_platform))
