39c6f894cde0be64141c3529a69f982168f5f9db
Timofei Medvedev <medtv78@gmail.com>
2024-07-03 17:50:54 +0300

    add all

--- find_number_platforms.py
+++ find_number_platforms.py
@@ -1,4 +1,4 @@
-"""Документация для кода ID115760654."""
+"""Документация для кода 115795162."""
 
 
 def find_number_platforms(
@@ -11,22 +11,15 @@ def find_number_platforms(
     while light_robot <= heavy_robot:
         weight_two_robots: int = sort_weight_robots[
             light_robot] + sort_weight_robots[heavy_robot]
-        if weight_two_robots <= limit_platform:
-            counter += 1
-            light_robot += 1
-            heavy_robot -= 1
-        else:
-            counter += 1
-            heavy_robot -= 1
+        light_robot += 1
+        counter += 1
+        heavy_robot -= 1
+        if weight_two_robots > limit_platform:
+            light_robot -= 1
     return counter
 
 
-# weight_robots = [int(x) for x in input().split()]
-# limit_platform = int(input())
-# weight_robots = [1, 2]
-# limit_platform = 3
-weight_robots = [3, 5, 3, 4]
-# limit_platform = 5weight_robots = [50, 50, 50, 50]
-limit_platform = 5
 if __name__ == '__main__':
+    weight_robots = [int(x) for x in input().split()]
+    limit_platform = int(input())
     print(find_number_platforms(weight_robots, limit_platform))
